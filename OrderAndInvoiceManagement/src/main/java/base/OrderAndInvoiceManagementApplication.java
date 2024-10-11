50b3a0b99bf22c63b111790f908e28305658d337
Shivam Mishra <ip6.sam@gmail.com>
2024-10-12 04:49:45 +0530

    Initial commit with basic setup

---
+++ OrderAndInvoiceManagement/src/main/java/base/OrderAndInvoiceManagementApplication.java
@@ -0,0 +1,13 @@
+package base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAndInvoiceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderAndInvoiceManagementApplication.class, args);
+	}
+
+}
